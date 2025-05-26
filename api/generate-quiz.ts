a428d4fff1776dc99bc590b34c0da9691b8fc058
mauro3x3 <maurokjaer@gmail.com>
2025-05-26 16:10:58 +0200

    should work now ffs

--- api/generate-quiz.ts
+++ api/generate-quiz.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
